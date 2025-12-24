b8aeb88491836f043fbc4bcb1e408684dcc25c37
Keunwoo <104286190+kw9212@users.noreply.github.com>
2025-12-24 18:11:54 +0900

    chore: add interceptor for success response

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { SuccessResponseInterceptor } from './common/interceptors/success-response-interceptor.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new SuccessResponseInterceptor());
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+await bootstrap();
