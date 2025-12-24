f14a191a25e6ce44886a9c4ecaeb4a34ee98ea4a
Keunwoo <104286190+kw9212@users.noreply.github.com>
2025-12-24 19:49:07 +0900

    chore: add common API response and error types

--- src/common/interceptors/success-response-interceptor.ts
+++ src/common/interceptors/success-response-interceptor.ts
@@ -1,11 +1,7 @@
+import { ApiSuccessResponse } from './../types/api-error-type.js';
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
-export type SuccessResponse<T> = {
-  ok: true;
-  data: T;
-};
 
 function isSuccessResponse(value: unknown): value is { ok: true; data: unknown } {
   if (typeof value !== 'object' || value === null) return false;
@@ -20,7 +16,7 @@ export class SuccessResponseInterceptor implements NestInterceptor {
   intercept(
     _context: ExecutionContext,
     next: CallHandler
-  ): Observable<SuccessResponse<unknown> | { ok: true; data: unknown }> {
+  ): Observable<ApiSuccessResponse<unknown> | { ok: true; data: unknown }> {
     return next
       .handle()
       .pipe(map((data: unknown) => (isSuccessResponse(data) ? data : { ok: true, data })));
