d64c31339fc43dc44c8c70c39d5d247aebab48c3
Keunwoo <104286190+kw9212@users.noreply.github.com>
2025-12-24 21:28:16 +0900

    chore: enable global validation pipe and CORS

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { SuccessResponseInterceptor } from './common/interceptors/success-response-interceptor.js';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter.js';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new SuccessResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
