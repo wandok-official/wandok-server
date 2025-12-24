0682e2197cfed104c13aa1a2fe3b763aab6021b9
Keunwoo <104286190+kw9212@users.noreply.github.com>
2025-12-24 18:11:54 +0900

    chore: add interceptor for success response

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { HealthcheckController } from './health/health.controller.js';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HealthcheckController],
   providers: [AppService],
 })
 export class AppModule {}
