62fb43254142a3cd667cafd34f161b2dbbe60a9a
HwangGoeun <hwang_gim@naver.com>
2025-12-28 15:42:02 +0900

    fix: Return 401 status code on JWT authentication failure

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service.js';
 import { users } from '../generated/prisma/client.js';
 
@@ -44,7 +44,7 @@ export class UsersService {
     });
 
     if (!user) {
-      throw new NotFoundException(`User with ID ${id} not found`);
+      throw new UnauthorizedException('Invalid credentials');
     }
 
     return {
